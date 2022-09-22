932ab59bb8af4429e68f668bab6ba307267a816f
dlepoutre <114146920+dlepoutre@users.noreply.github.com>
2022-09-22 12:11:03 +0200

    Fix DemoApplication

--- src/main/java/com/reytech/demo/DemoApplication.java
+++ src/main/java/com/reytech/demo/DemoApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DemoApplication {
 	
-	@Autowired
-	private Singleton singleton;
+	//@Autowired
+	//private Singleton singleton;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
