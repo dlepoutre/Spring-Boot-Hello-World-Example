12d300d336ffb377c7ec3210a04baba44ee0f4c8
dlepoutre <114146920+dlepoutre@users.noreply.github.com>
2022-09-22 14:09:52 +0200

    Remove comment

--- src/main/java/com/reytech/demo/DemoApplication.java
+++ src/main/java/com/reytech/demo/DemoApplication.java
@@ -8,7 +8,6 @@ public class DemoApplication {
 	
 	//@Autowired
 	//private Singleton singleton;
-	//
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
