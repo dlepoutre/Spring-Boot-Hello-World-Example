7f8e3a51bf5a8e388833e6309fed1f8557994279
dlepoutre <114146920+dlepoutre@users.noreply.github.com>
2022-09-22 12:08:02 +0200

    Add autowired singleton dependency

--- src/main/java/com/reytech/demo/DemoApplication.java
+++ src/main/java/com/reytech/demo/DemoApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
+	
+	@Autowired
+	private Singleton singleton;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
